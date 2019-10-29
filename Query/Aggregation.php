5d36d78d62cb26940268c5bdc59ce6098339efba
Stefan Csizmazia <s.csizmazia@gmail.com>
2019-10-29 12:49:21 +0100

    add support for global aggregations

--- Query/Aggregation.php
+++ Query/Aggregation.php
@@ -15,6 +15,11 @@ use InvalidArgumentException;
 class Aggregation implements AggregationInterface
 {
     /**
+     * @see https://www.elastic.co/guide/en/elasticsearch/reference/current/search-aggregations-bucket-global-aggregation.html
+     */
+    public const GLOBAL = 'global';
+
+    /**
      * @var string
      */
     protected $name;
@@ -47,7 +52,7 @@ class Aggregation implements AggregationInterface
      */
     public function __construct(string $field, string $type, string $name = '', array $options = [])
     {
-        if (!Metric::hasConstant($type) && !Bucket::hasConstant($type)) {
+        if (!Metric::hasConstant($type) && !Bucket::hasConstant($type) && $type !== static::GLOBAL) {
             throw new InvalidArgumentException('Unknown type "' . $type . '" given');
         }
 
@@ -69,9 +74,20 @@ class Aggregation implements AggregationInterface
      *
      * @return \App\Services\Elasticsearch\Query\Aggregation
      */
-    public static function create(string $field, string $type, string $name = '', array $options = [])
+    public static function create(string $field, string $type, string $name = '', array $options = []): Aggregation
     {
         return new self($field, $type, $name, $options);
+    }
+
+    /**
+     * @param string $name
+     * @param array  $options
+     *
+     * @return \App\Services\Elasticsearch\Query\Aggregation
+     */
+    public static function createGlobal(string $name = '', array $options = []): Aggregation
+    {
+        return new self('', static::GLOBAL, $name, $options);
     }
 
     /**
@@ -99,14 +115,23 @@ class Aggregation implements AggregationInterface
      */
     public function toArray(): array
     {
-        $body = [
-            $this->name => [
-                $this->type => array_merge(
-                    ['field' => $this->field],
+        if ($this->type === static::GLOBAL) {
+            $body = [
+                $this->name => array_merge(
+                    ['global' => new \stdClass()],
                     $this->options
                 ),
-            ],
-        ];
+            ];
+        } else {
+            $body = [
+                $this->name => [
+                    $this->type => array_merge(
+                        ['field' => $this->field],
+                        $this->options
+                    ),
+                ],
+            ];
+        }
 
         if (count($this->nestedAggregations) > 0) {
             $body[$this->name]['aggs'] = array_reduce(
